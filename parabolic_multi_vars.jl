90c9af2cf6367d35fe84ab3cf3b394d38b113dec
Maxim <53125181+Maximka-hue@users.noreply.github.com>
2023-05-19 14:59:09 +0300

    Update parabolic_multi_vars.jl

--- parabolic_multi_vars.jl
+++ parabolic_multi_vars.jl
@@ -1,12 +1,14 @@
 #Symbolic representation of derivatives, etc.
-using SymEngine, BenchmarkTools
-using CartesianGrids, Base.Cartesian #Meshes,Convex
+#Meshes,Convex, SymEngine, CartesianGrids
+using BenchmarkTools
+using Base.Cartesian
 using Distributions, LinearAlgebra, Distances, Calculus, SpecialFunctions, LaTeXStrings
 using Printf, DataFrames, DelimitedFiles
 using Symbolics, MLStyle, ToggleableAsserts
 using Glob, JSON, JSONTables, CSV, Plots;
 include("default_variables.jl")
 include("travrse_prep.jl")
+include("multips_str.jl")#p-parameters, s -steps
 #include.(filter(contains(r".jl$"), readdir(pwd(); join=true)))
 """
 The structure of the consistent use of the program set as follows:
@@ -16,7 +18,7 @@ The structure of the consistent use of the program set as follows:
 2 Derived parameters are set
 3 Based on number the appropriate overloads are chosen for
    traversing the grid
-4 the resulting images and description are set in proper
+4 the resulting images and descriptions are set in proper
    directories for each set of parameters
 
     axissymmetric_parabolic_scheme(*) - solves the 1D problem in polar coordinates
@@ -29,13 +31,14 @@ The structure of the consistent use of the program set as follows:
 #read_parameters()
 prpk, each_varying = init_from_files()
 #Default constant(linear proportionality) and the parameter
-#indicating the degree of the speed upon nonlinear dependence
-a, α = 3, 0.3
-#Convinience parameter and specifying porosity(constant (porosity))
-m = 0.1
+#indicating the degree of the speed upon nonlinear dependence(Doesn't need to be so)
+#m-porosity, a- constants(doesn't necessary but on the off-chance)
+m, a, α = 0.1, 3, 0.3
+
 for (k, v) in each_varying
     println("varying parameter: $k, value: $v")
 end
+#It's simpler to assign explicitly
 m, α, a  = each_varying["m"], each_varying["α"], each_varying["a"]
 
 all_params_number = prpk.params_num
@@ -69,107 +72,56 @@ _varalpha = 0.3
 if prpk.chosen_modes[:multiple_trials]
     Nh, Mv = 0, 0
 else
+    #Nh, Mv - steps without right boundary condition
     Nh, Mv = prpk.initial_model_values["Nh"], prpk.initial_model_values["Mv"]
 end
 
-rstep_size, Pdf, Pddf, mhgrid, mvgrid =choose_numgrid(hsize, vsize, initial_step_value, expiremntal_increase, step_max, Nh, Mv,
+rstep_size, Pdf, Pddf, mhgrid, mvgrid = choose_numgrid(hsize, vsize, initial_step_value, expiremntal_increase, step_max, Nh, Mv, a,
     multiple_trials, false)
 (debug_output && println(rstep_size, Pdf, Pddf, mhgrid, mvgrid))
-init_shape =""
+init_shape =" "
 #From this point I am also would like to save this parameters in newly created for the form directory
 #But I need number of ParametersPack's used, so I am creating beforehand all of them
 #ll = mkpath(anim_dir*"$(init_shape)_ijk\\")
 #print(ispath(ll))
 #println( ".... $anim_dir ... $(init_shape)_ijk ... ")
-#=
-if multiple_parameters
-    init_shape = "\\"
-    println(mkpath(anim_dir*"$(init_shape)_ijk\\"), ".... $anim_dir ... $(init_shape)_ijk ... ")
-    for i in size(a)[1], j = size(α)[1], k = size(m)[1]
-        global init_shape
-        println("Diriectory n: ", i, j, k)
-        if initial_shape == 1
-            init_shape = "Plane"
-        elseif initial_shape == 2
-            init_shape = "Sphere"
-        elseif initial_shape == 3
-            init_shape = "NormalDistribution"
-        else
-            init_shape = "smth_sh"
-        end
-        if same_then_reduce && size(a)[1] == size(α)[1] == size(m)[1] && i ==j ==k
-            to_create = joinpath(data_dir, "$(init_shape)_ijk")
-            mkdir(to_create)
-        else
-            #For each a, m, α separetely
-            #anim_dirn = mkpath(anim_dir*"$(init_shape)_ijk\\")
-            #data_dirn = mkpath(data_dir*"$(init_shape)_ijk\\")
-            to_create_anim = joinpath(anim_dir, "$(init_shape)_ijk\\")
-            to_create_data = joinpath(data_dir, "$(init_shape)_ijk\\")
-            debug_output && (println("Directory $to_create_anim or $to_create_data exists?"); isdir(anim_dirn) && isdir(data_dir))
-            mkdir(anim_dirn)
-            mkdir(data_dirn)
-        end
-    end
-#I hope don't need
-elseif multiple_trials
-    for i in 1:rstep_size
-        to_create = joinpath(data_dir, "$(init_shape)_step$i")
-        mkdir(to_create)
-    end
-end
-=#
-
-#Now lets read file data or leave the default ones
+#Now lets read file data or leave the default ones by my chose
 initial_dir = abspath("C:/Users/2020/Desktop/CurrentReading/AsideMy/RCJsPyJulia_projects/ConcreteFluidSystem/ParabolicEquation")
 if pwd() == "C:\\Users\\2020" && Sys.iswindows()
     cd(initial_dir)
     #cd("C:/Users/2020")
 end
 println("\nCurrent directory with program: \n", pwd())
-#------------------------------------------------------------------------------------------------------------------
+
 println("Pdf:   ", eltype.(eachcol(Pdf)))
 println("Pddf:   ", eltype.(eachcol(Pddf)))
+#Sooner I need to write them in Result directory
 CSV.write("string_primary_data.csv", Pdf)
 CSV.write("model_data.csv", Pddf)
+num_multiparams = prpk.params_num
+mps::MPS = MPS(num_multiparams, num_consts, rand_steps,
+    rstep_size = rand_steps, sizes_of_parameters)
+#According to specification of Base.*, write dependence of the flow based on the overloads of functions
+if multiple_parameters && multiple_trials
+    write_dirs_according_to_varying_parameters(a, α, m, mhgrid, mvgrid; same_then_reduce=true, multiple_trials=true)
+    #determine_initial_surface_shape(initial_shape,
+elseif multiple_parameters #One-StepSet-Multi-Parameter - my case for simplicity
+    write_dirs_according_to_varying_parameters(a, α, m ;same_then_reduce=true)
+else
+    #Must be default to One-StepSet-One-ParametersSet
+    write_dirs_according_to_varying_parameters()
+    #determine_initial_surface_shape(initial_shape, NL_1, NH, MV, h, τ, (LD, RD, LU, RU))
+end
+
+#CSV.write(
+#------------------------------------------------------------------------------------------------------------------
+#=
 (debug_time_sleep && sleep(1))
 Nh, Mv = Int64(Nh), Int64(Mv)
 NH = Nh + 1
 MV = Mv + 1
-num_multiparams = prpk.params_num
-#Come into play in multvariable case
-Base.@kwdef mutable struct MPS
-    #Multistep form
-        SLs::Array{Float64, 2} = zeros(Float64, rstep_size, NH)
-        SRs::Array{Float64, 2} = zeros(Float64, rstep_size, MV)
-    #Multiparameter separate form
-        PLs::Array{Float64, 2} = zeros(Float64, num_multiparams, NH)
-        PRs::Array{Float64, 2} = zeros(Float64, num_multiparams, MV)
-    #Other changable const
-        ALs::Array{Float64, 2} = zeros(Float64, num_multiparams, NH)
-        ARs::Array{Float64, 2} = zeros(Float64, num_multiparams, MV)
-end
-function Base.getproperty(mps::MPS, p::Symbol)
-    if p == :sls
-        return getfield(mps, :SLs)   # getfield() reads fields directly and it cannot be overloaded
-    elseif p == :srs
-        return getfield(mps, :SRs)
-    elseif p == :pls
-        return getfield(mps, :PLs)
-    elseif p == :prs
-        return getfield(mps, :PRs)
-    elseif p == :als
-        return getfield(mps, :ALs)
-    elseif p == :ars
-        return getfield(mps, :ARs)
-    else
-        return getfield(mps, p)
-    end
-end
 
-if multiple_trials || multiple_parameters
-    MPS_ = MPS()
-end
+
 
 if !multiple_trials
     #Initialization of used array variables
@@ -250,7 +202,7 @@ initial_const = 0.0
 (debug_time_sleep && println("U[1] type- $(u[1, :]), NL_1 type- $NL_1,\n and sizes: $(sizeof(NL_1)) ... $(sizeof(u[1, :]))"))
 u[1, :]= deepcopy(NL_1)
 w[1, :]= deepcopy(NL_1)
-println("By reference?: $(NL_1 == NL[1])")
+(debug_output && println("By reference?: $(NL_1 == NL[1])"))
 
 #symmetric_layer_fill!(NL_2, NL_1, 2)
 axisymmetric_parabolic_scheme(u, #timesteps::Integer,
@@ -259,8 +211,8 @@ axisymmetric_parabolic_scheme(u, #timesteps::Integer,
 @macroexpand @nref 2 NL i
 #Additionly create plots from the resulting 2-dim massive
 
-
 new_prpk = ParametersPack{Float64}()
 copy!(new_prpk, prpk)#ParametersPack{Float64}(prpk.initial_model_values, params_num, control_b, pts)
 println(new_prpk, "\n", prpk)
  #dst.chosen_modes
+=#
